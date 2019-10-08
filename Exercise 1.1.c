11f6736ffa4c2174972f6f7ff3774cb28f3e0a80
Marianne Lawless <lawlessmarianne@gmail.com>
2019-10-08 01:07:00 +0100

    Exercise 1.1 added

---
+++ Exercise 1.1.c
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?> 
+
+<library>	
+	<catalogue category ="technical">
+	  <book>
+		<ISBN>9781118008188</ISBN>
+		<Title>HTML & CSS</Title>
+		<Author>Jon Ducket</Author>
+	  </book>
+		<ISBN>978118531648</ISBN>
+		<Title>JAVASCRIPT & JQUERY</Title>
+		<Author>Jon Ducket</Author>
+	</book>
+	 </catalogue>
+	<catalogue category ="cookery">
+	<book>
+		<ISBN>0718187768 </ISBN>
+		<Title>Veg</Title>
+		<Author>Jamie Oliver</Author>
+	</book>
+	<book>
+		<ISBN>0718187768 </ISBN>
+		<Title>Veg</Title>
+		<Author>Jamie Oliver</Author>
+	</book>
+	 </catalogue>	 
+	
+</Library>
+
