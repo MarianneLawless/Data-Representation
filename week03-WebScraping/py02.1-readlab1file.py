ea2c8c750095cffd2324566e94621bfb6666e76b
Marianne Lawless <lawlessmarianne@gmail.com>
2019-10-19 15:10:50 +0100

    Worked on Web Scraping

--- week03-WebScraping/py02.1-readlab1file.py
+++ week03-WebScraping/py02.1-readlab1file.py
@@ -1,6 +1,8 @@
+#Test that you can read a file , using lab1.html file tht we mde last week 
+
 from bs4 import BeautifulSoup
 
 with open("../lab1.html") as fp:
     soup = BeautifulSoup(fp,'html.parser')
 
-print (soup.prettify())
+print(soup.prettify())
