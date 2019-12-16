9a2994081d73d53861231bdd039363dc22d7df5e
Marianne Lawless <lawlessmarianne@gmail.com>
2019-12-16 00:38:27 +0000

    added labs

---
+++ Week06/lab06.02-gitaccess.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+apiKey = '2c9b7439c13cd1787826a685bc11a31e13164e0c'
+url = 'https://api.github.com/repos/MarianneLawless/PrivateRepo'
+filename ="repo.json"
+
+response = requests.get(url, auth=('token',apiKey))
+
+repoJSON = response.json()
+#print (response.json())
+
+file = open(filename, 'w')
+json.dump(repoJSON, file, indent=4)
